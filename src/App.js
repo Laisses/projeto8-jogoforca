eab67e6815b5b2149c1ea919378790ce56d17ea5
Laisses <71999249+Laisses@users.noreply.github.com>
2022-09-20 14:32:11 -0300

    add App componenent with basic structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React from "react";
-import {words} from "./words";
+import { words } from "./words";
 
 export const App = () => {
-    return (<h1>Stuff</h1>);
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+    const Letter = props => {
+        return (
+            <li>{props.name}</li>
+        );
+    };
+
+
+    return (
+        <main className="main">
+            <div className="game">
+                <img src="assets/forca0.png" alt="imagem da forca" className="game__image" />
+                <button className="game__choose-button">Escolher Palavras</button>
+            </div>
+            <ul className="letters">
+                {alphabet.map(letter => <Letter key={letter} name={letter} />)}
+            </ul>
+            <div>
+                <p>Já sei a resposta!</p>
+                <input type="text" name="chute" className="chute" />
+                <button>Chutar</button>
+            </div>
+        </main>
+    );
 };
