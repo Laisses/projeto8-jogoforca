10887b48514f6782957718052698813e9b593528
Laisses <71999249+Laisses@users.noreply.github.com>
2022-09-22 16:54:56 -0300

    change useState to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import { Guess } from "./Guess";
+import { Letters } from "./Letters";
 import { Game } from "./Game";
+import {words} from "./words";
 
-export const App = () => {    
+export const App = () => {
+    const initialGameState = {
+        word: null,
+        chances: 6,
+        KeyboardEnabled: false,
+    };
+
+    const [gameState, setGameState] = useState(initialGameState);
+
+    const randomizeWord = () => {
+        const wordsList = words;
+        return wordsList[Math.floor(Math.random() * wordsList.length)];
+    }
+
+    const chooseWord = () => {
+        const randomWord = randomizeWord();
+        setGameState({ ...gameState, word: randomWord, KeyboardEnabled: true });
+    }
+    
     return (
         <main className="main">
-            <Game />
+            <Game word={gameState.word} onClick={chooseWord}/>
+            <Letters enabled={gameState.KeyboardEnabled}/>
             <Guess />
         </main>
     );
--- src/Game.js
+++ src/Game.js
@@ -1,36 +1,11 @@
-import { useState } from "react";
-import { Letters } from "./Letters";
-import { words } from "./words";
 import forca0 from "./assets/forca0.png";
 
-const randomizeWord = () => {
-    const wordsList = words;
-    return wordsList[Math.floor(Math.random() * wordsList.length)];
-}
-
-const initialGameState = {
-    word: null,
-    chances: 6,
-    KeyboardEnabled: false,
-};
-
-export const Game = () => {
-
-    const [gameState, setGameState] = useState(initialGameState);
-
-    const chooseWord = () => {
-        const randomWord = randomizeWord();
-        setGameState({ ...gameState, word: randomWord, KeyboardEnabled: true });
-    }
-
+export const Game = ({ word, onClick }) => {
     return (
-        <>
-            <div className="game">
-                <img src={forca0} alt="imagem da forca" className="game__image" />
-                <button className="game__button" onClick={chooseWord}>Escolher Palavras</button>
-                <div className="game__word">{gameState.word}</div>
-            </div>
-            <Letters enabled={gameState.KeyboardEnabled}/>
-        </>
+        <div className="game">
+            <img src={forca0} alt="imagem da forca" className="game__image" />
+            <button className="game__button" onClick={onClick}>Escolher Palavras</button>
+            <div className="game__word">{word}</div>
+        </div>
     );
 };
