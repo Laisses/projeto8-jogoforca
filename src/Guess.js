03565a3ddba4989adca979e834e0cdda0e2bcd41
Laisses <71999249+Laisses@users.noreply.github.com>
2022-09-23 17:07:53 -0300

    handle enter key on Guess.js

--- src/Guess.js
+++ src/Guess.js
@@ -2,7 +2,15 @@ export const Guess = ({enabled, input, onClick, value}) => {
     return (
         <div className="guess">
             <p>Já sei a palavra!</p>
-            <input type="text" name="guess" className="guess__input" disabled={enabled ? false : true} onChange={input} value={value}/>
+            <input 
+                type="text" 
+                name="guess" 
+                className="guess__input" 
+                disabled={enabled ? false : true} 
+                onChange={input} 
+                value={value} 
+                onKeyPress={e => e.key === "Enter" && onClick()}
+            />
             <button className="guess__button" onClick={onClick}>Chutar</button>
         </div>
     );
