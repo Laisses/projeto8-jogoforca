058125e0c6fa3a2f4f646fddfdb63d155b60e52b
Laisses <71999249+Laisses@users.noreply.github.com>
2022-09-26 08:30:59 -0300

    remove console.log from App

--- src/App.js
+++ src/App.js
@@ -43,8 +43,6 @@ export const App = () => {
             input: "",
             button: "Mudar Palavra"
         });
-
-        console.log(randomWord);
     };
 
     const simplifyLetter = l => {
